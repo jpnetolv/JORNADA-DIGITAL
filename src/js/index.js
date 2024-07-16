002a8a6b14780bed97ef402d98ee99fc07b1029b
jpneto_ <jpnetotest@gmail.com>
2024-07-16 10:36:54 -0300

    commit

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+// Objetivo 1 - quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html
+
+// Passo 1 - pegar o botão mostrar mais no JS para poder verificar quando o usuário clicar em cima dele
